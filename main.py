c8b63183181ce3d1b1c7c526b4f51d523f6a2c12
Emily Y <emilygbs@yahoo.com>
2019-07-25 15:27:53 -0400

    added dictionary for image to coco_urls

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ def main():
     path_to_json = "captions_train2014.json"
     with open(path_to_json, "rb") as f:
         json_data = json.load(f)
-    #documents = []
+    documents = []
     img_to_caption = {}
+    img_to_coco = {}
     for caption in json_data['annotations']:
         img_id = caption['image_id']
-        print(img_id)
         if img_id in img_to_caption:
             img_to_caption[img_id].append(caption['caption'])
         else:
             img_to_caption[img_id] = []
-    #for caption in range(82783):
-    #    documents.append(json_data['annotations'][caption]['caption'])
-    #counters = [embed_text.to_counter(doc) for doc in documents]
-    #vocab = embed_text.to_vocab(counters)
-    #idfs = embed_text.to_idf(vocab, counters)
-    print(img_to_caption[133071])
+    for image in json_data['images']:
+        img_to_coco[image['id']] = image['coco_url']
+    for caption in range(82783):
+        documents.append(json_data['annotations'][caption]['caption'])
+    counters = [embed_text.to_counter(doc) for doc in documents]
+    vocab = embed_text.to_vocab(counters)
+    idfs = embed_text.to_idf(vocab, counters)
 
 if __name__ == "__main__":
     main()
