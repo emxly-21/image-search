f21004a3d8d34a27f4af492a9f70d151102b94ce
Darshan <darsh797@gmail.com>
2019-07-25 13:55:05 -0400

    Tested unpickle. Made docstring nicer.

--- unpickle.py
+++ unpickle.py
@@ -2,12 +2,15 @@ import pickle
 
 
 def unpickle():
-    '''
 
-    Unpickles the resnet18_features.pkl file to a dictionary and returns the dictionary.
+    """
 
-    :return: pFile: a dictionary with is obtained by unpickling the resnet19_features.pkl file
-    '''
+    Unpickles the resnet18_features.pkl file and saves it into a dictionary called pfile
+
+    Returns pfile, a dictionary containing the content of the resnet18_features.pkl
+    -------
+
+    """
     with open('resnet18_features.pkl', mode='rb') as file:
-        pFile = pickle.load(file)
-    return pFile
+        pfile = pickle.load(file)
+    return pfile
