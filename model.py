ce9562a6b47928cb9fdca37b141720ab28983fce
David Qian <david.qian02@gmail.com>
2019-07-25 15:45:23 -0400

    model edits

--- model.py
+++ model.py
@@ -1,47 +1,40 @@
 from mynn.layers.dense import dense
-
+from mygrad.nnet import margin_ranking_loss
 
 class Model:
     def __init__(self):
         self.dense1 = dense(512, 50)
 
     def __call__(self, x):
-        ''' Forward data through the network.
-
-        This allows us to conveniently initialize a model `m` and then send data through it
-        to be classified by calling `m(x)`.
-
-        Parameters
-        ----------
-        x : Union[numpy.ndarray, mygrad.Tensor], shape=(N, D)
-            The data to forward through the network.
-
-        Returns
-        -------
-        mygrad.Tensor, shape=(N, 1)
-            The model outputs.
-        '''
         return self.dense1(x)
 
     @property
     def parameters(self):
-        ''' A convenience function for getting all the parameters of our model. '''
         return self.dense1.parameters
-
+import mygrad as mg
+#from mygrad import Tensor
+def accuracy(simgood,simbad):
+    return simgood-simbad
 
 from mynn.optimizers.sgd import SGD
+id1=np.random.randint(0, 82784)
+id2=np.random.randint(0, 82784)
+while id1 == id2:
+    id2=np.random.randint(0, 82784)
+good_image = resnet[id1]
+text = img_to_caption[id1]
+bad_image =resnet[id2]
 model=Model()
 optim = SGD(model.parameters, learning_rate=0.1)
-for epoch_cnt in range(100):
-    good_image = np.
-    text =
-    bad_image =
+
+for epoch_cnt in range(10000):
+
     sim_to_good = sim(se_text(text), model(good_image))
     sim_to_bad = sim(se_text(text), model(bad_image))
 
     # compute the loss associated with our predictions(use softmax_cross_entropy)
-    loss = margin_ranking_loss(sim_good, sim_bad, 1, 0.1)
-
+    loss = margin_ranking_loss(sim_to_good, sim_to_bad, 1, 0.1)
+    acc=accuracy(sim_to_good,sim_to_bad)
     # back-propagate through your computational graph through your loss
     loss.backward()
 
