784ad7a997b0134b1cd06e03e3d40d7612b3dd94
Jessie <jessiexxcheung@gmail.com>
2019-07-25 15:24:11 -0400

    kjl

---
+++ display_images.py
@@ -0,0 +1,18 @@
+def display_imgs(image_ids, ids_to_url):
+    """
+    Plots the images of the image_ids
+    :param image_ids: an Iterable of ids
+    :param ids_to_url: Dict[ids: tuple(captions, coco_url)
+    :return: None
+    """
+    for image in image_ids:
+        url = ids_to_url[image][1]
+        data = urlopen(url)
+
+        # converting the downloaded bytes into a numpy-array
+        url_format = url[url.rfind('.')+1:]
+        img = plt.imread(data, format=url_format)  # shape-(460, 640, 3)
+
+        # displaying the image
+        fig, ax = plt.subplots()
+        ax.imshow(img)
