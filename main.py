868c35fbab0291e4c27c4d0f88ecce9ebdb643fe
Emily Y <emilygbs@yahoo.com>
2019-07-26 10:50:43 -0400

    fixed to account for words not in glove

--- main.py
+++ main.py
@@ -4,50 +4,14 @@ import json
 import unpickle
 import numpy as np
 import pickle
-
+import make_database
 
 def main():
     path = r"glove.6B.50d.txt.w2v"
     glove = KeyedVectors.load_word2vec_format(path, binary=False)
 
-    # loads the json file
-    path_to_json = "captions_train2014.json"
-    with open(path_to_json, "rb") as f:
-        json_data = json.load(f)
     resnet = unpickle.unpickle()
-
-    documents = []
-    img_to_caption = {}
-    img_to_coco = {}
-
-    # creates a Dict[img_ids: captions]
-    for caption in json_data['annotations']:
-        img_id = caption['image_id']
-        if img_id in img_to_caption:
-            img_to_caption[img_id].append(caption['caption'])
-        else:
-            img_to_caption[img_id] = []
-
-    # creates a Dict[img_ids: coco_url]
-    for image in json_data['images']:
-        img_to_coco[image['id']] = image['coco_url']
-    for caption in range(82783):
-        documents.append(json_data['annotations'][caption]['caption'])
-
-    counters = [embed_text.to_counter(doc) for doc in documents]
-    vocab = embed_text.to_vocab(counters)
-    idfs = embed_text.to_idf(vocab, counters)
-
-    # Saves idfs into a pickle file
-    with open('idfs.pkl', mode='wb') as file:
-        pickle.dump(idfs, file)
-    # Saves img_to_caption into a pickle file
-    with open('img_to_caption.pkl', mode='wb') as file:
-        pickle.dump(img_to_caption, file)
-    # Saves img_to_coco into a pickle file
-    with open('img_to_coco.pkl', mode='wb') as file:
-        pickle.dump(img_to_coco, file)
-
+    # make_database.make_database()     # uncomment this only if you want to repickle the files
 
 if __name__ == "__main__":
     main()
