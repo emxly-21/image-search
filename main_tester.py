9bd266843b94d17c55903cf9730b2a080ee5f4a4
David Qian <david.qian02@gmail.com>
2019-07-26 09:59:55 -0400

    hi

--- main_tester.py
+++ main_tester.py
@@ -1,13 +1,16 @@
 from gensim.models.keyedvectors import KeyedVectors
 import embed_text
 import sim
+import mygrad as mg
+from mygrad import Tensor
 import json
 import unpickle
 import numpy as np
 from mynn.optimizers.sgd import SGD
 from mynn.layers.dense import dense
 from mygrad.nnet import margin_ranking_loss
-
+from sklearn.metrics.pairwise import cosine_similarity
+import cos_sim
 
 def main():
     path = r"glove.6B.50d.txt.w2v"
@@ -31,7 +34,7 @@ def main():
         else:
             img_to_caption[img_id] = []
 
-     #creates a Dict[img_ids: coco_url]
+    # creates a Dict[img_ids: coco_url]
     for image in json_data['images']:
         img_to_coco[image['id']] = image['coco_url']
     for caption in range(82783):
@@ -41,11 +44,8 @@ def main():
     vocab = embed_text.to_vocab(counters)
     idfs = embed_text.to_idf(vocab, counters)
 
-
-
     model = Model()
     optim = SGD(model.parameters, learning_rate=0.1)
-    batch_size = 100
 
     for i in range(100):
         id1 = np.random.choice(list(resnet.keys()))
@@ -53,26 +53,26 @@ def main():
         id2 = np.random.choice(list(resnet.keys()))
         while id1 == id2:
             id2 = np.random.choice(list(resnet.keys()))
-        good_image = resnet[id1]
-        text = img_to_caption[id1][0]
-        bad_image = resnet[id2]
-        sim_to_good = sim.sim(embed_text.se_text(text, glove, idfs), model(good_image))
-        sim_to_bad = sim.sim(embed_text.se_text(text, glove, idfs), model(bad_image))
+        print(type(resnet[id1]),type(img_to_caption[id1][0]),type(resnet[id2]))
+        good_image = mg.Tensor(resnet[id1])
+        text = (img_to_caption[id1][0])
+        bad_image = mg.Tensor(resnet[id2])
+
+        sim_to_good = cos_sim.cos_sim(model(good_image), embed_text.se_text(text, glove, idfs))
+        sim_to_bad = cos_sim.cos_sim(model(bad_image), embed_text.se_text(text, glove, idfs))
 
     # compute the loss associated with our predictions(use softmax_cross_entropy)
         loss = margin_ranking_loss(sim_to_good, sim_to_bad, 1, 0.1)
-        acc = accuracy(sim_to_good, sim_to_bad)
     # back-propagate through your computational graph through your loss
         loss.backward()
-
     # compute the accuracy between the prediction and the truth
-
+        acc = accuracy(sim_to_good, sim_to_bad)
     # execute gradient descent by calling step() of optim
         optim.step()
-
     # null your gradients
         loss.null_gradients()
     print(acc)
+
 
 class Model:
     def __init__(self):
@@ -84,10 +84,11 @@ class Model:
     @property
     def parameters(self):
         return self.dense1.parameters
-import mygrad as mg
-#from mygrad import Tensor
-def accuracy(simgood,simbad):
+
+
+def accuracy(simgood, simbad):
     return simgood-simbad
+
 
 if __name__ == "__main__":
     main()
