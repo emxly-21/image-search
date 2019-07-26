688d7cd2f38151efc1bb46a424a3927406e60d80
David Qian <david.qian02@gmail.com>
2019-07-26 16:30:07 -0400

    IT FINALLY WORKS

--- main_tester.py
+++ main_tester.py
@@ -11,6 +11,7 @@ from mynn.layers.dense import dense
 from mygrad.nnet import margin_ranking_loss
 import cos_sim
 import pickle
+from mynn.optimizers.adam import Adam
 
 def main():
     path = r"glove.6B.50d.txt.w2v"
@@ -30,41 +31,54 @@ def main():
         #img_to_coco=pickle.load(coco)
     model = Model()
 
-    model.dense1.weight=mg.Tensor(np.load('weights.npy'))
-    model.dense1.bias=mg.Tensor(np.load('bias.npy'))
-    optim = SGD(model.parameters, learning_rate=0.1)
-    accarr=[]
-    for i in range(100):
-        id1 = np.random.choice(list(resnet.keys()))
-        #print(id1)
-        id2 = np.random.choice(list(resnet.keys()))
-        while id1 == id2:
-            id2 = np.random.choice(list(resnet.keys()))
+    model.dense1.weight = mg.Tensor(np.load('weight.npy'))
+    model.dense1.bias = mg.Tensor(np.load('bias.npy'))
+    optim = Adam(model.parameters)
 
-        #print(type(resnet[id1]),type(img_to_caption[id1][0]),type(resnet[id2]))
-        good_image = mg.Tensor(resnet[id1])
-        text = (img_to_caption[id1][0])
-        bad_image = mg.Tensor(resnet[id2])
+    batch_size = 100
+    for epoch_cnt in range(100):
 
-        sim_to_good = cos_sim.cos_sim(model(good_image), embed_text.se_text(text, glove, idfs))
-        sim_to_bad = cos_sim.cos_sim(model(bad_image), embed_text.se_text(text, glove, idfs))
+        idxs = list(resnet.keys())
+        np.random.shuffle(idxs)
+        for batch_cnt in range(0, len(idxs) // batch_size - 1):
+            batch_indices = idxs[(batch_cnt * batch_size):((batch_cnt + 1) * batch_size)]
+            batch_indices2 = idxs[((batch_cnt + 1) * batch_size):((batch_cnt + 2) * batch_size)]
+            # id1 = np.random.choice(list(resnet.keys()))
+            # print(id1)
+            id1 = batch_indices
+            # while id1 == id2:
+            id2 = batch_indices2
+
+            # print(type(resnet[id1]),type(img_to_caption[id1][0]),type(resnet[id2]))
+            good_image = resnet[id1[0]]
+            bad_image = resnet[id2[0]]
+            text = embed_text.se_text(img_to_caption[id1[0]][0], glove, idfs)
+            for i in id1[1:]:
+                good_image = np.vstack((good_image, resnet[i]))
+                text = np.vstack((text, embed_text.se_text(img_to_caption[i][0], glove, idfs)))
+
+            for i in id2[1:]:
+                bad_image = np.vstack((bad_image, resnet[i]))
+
+            sim_to_good = cos_sim.cos_sim(model(good_image), text)
+            sim_to_bad = cos_sim.cos_sim(model(bad_image), text)
+
+            # compute the loss associated with our predictions(use softmax_cross_entropy)
+            loss = margin_ranking_loss(sim_to_good, sim_to_bad, 1, 0.1)
+            # back-propagate through your computational graph through your loss
+            loss.backward()
+
+            # compute the accuracy between the prediction and the truth
+            acc = accuracy(sim_to_good.data, sim_to_bad.data)
+            # execute gradient descent by calling step() of optim
+            optim.step()
+            # null your gradients
+            loss.null_gradients()
 
 
+    np.save('weight', model.dense1.parameters[0].data)
+    np.save('bias', model.dense1.parameters[1].data)
 
-    # compute the loss associated with our predictions(use softmax_cross_entropy)
-        loss = margin_ranking_loss(sim_to_good, sim_to_bad, 1, 0.1)
-    # back-propagate through your computational graph through your loss
-        loss.backward()
-    # compute the accuracy between the prediction and the truth
-        acc = accuracy(sim_to_good, sim_to_bad)
-    # execute gradient descent by calling step() of optim
-        optim.step()
-    # null your gradients
-        loss.null_gradients()
-        accarr.append(1 if acc>0 else 0)
-    np.save('weight',model.dense1.parameters[0].data)
-    np.save('bias',model.dense1.parameters[1].data)
-    print(sum(accarr)/100)
 
 class Model:
     def __init__(self):
@@ -79,7 +93,7 @@ class Model:
 
 
 def accuracy(simgood, simbad):
-    return simgood-simbad
+    return np.mean(simgood - simbad > 0)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -5,47 +5,69 @@ import numpy as np
 import unpickle
 
 class Model:
-    def __init__(self):
-        self.dense1 = dense(512, 50)
+def __init__(self):
+    self.dense1 = dense(512, 50)
 
-    def __call__(self, x):
-        return self.dense1(x)
+def __call__(self, x):
+    return self.dense1(x)
 
-    @property
-    def parameters(self):
-        return self.dense1.parameters
-import mygrad as mg
-#from mygrad import Tensor
-def accuracy(simgood,simbad):
-    return simgood-simbad
+@property
+def parameters(self):
+    return self.dense1.parameters
 
 
-id1=np.random.randint(0, 82784)
-id2=np.random.randint(0, 82784)
-while id1 == id2:
-    id2=np.random.randint(0, 82784)
-good_image = resnet[id1]
-text = img_to_caption[id1]
-bad_image =resnet[id2]
-model=Model()
-optim = SGD(model.parameters, learning_rate=0.1)
+def accuracy(simgood, simbad):
+    return np.mean(simgood - simbad > 0)
 
-for epoch_cnt in range(10000):
 
-    sim_to_good = sim(se_text(text), model(good_image))
-    sim_to_bad = sim(se_text(text), model(bad_image))
 
-    # compute the loss associated with our predictions(use softmax_cross_entropy)
-    loss = margin_ranking_loss(sim_to_good, sim_to_bad, 1, 0.1)
-    acc=accuracy(sim_to_good,sim_to_bad)
-    # back-propagate through your computational graph through your loss
-    loss.backward()
+model = Model()
 
-    # compute the accuracy between the prediction and the truth
-    acc = accuracy(prediction, truth)
+#model.dense1.weight = mg.Tensor(np.load('weight.npy'))
+#model.dense1.bias = mg.Tensor(np.load('bias.npy'))
+optim = Adam(model.parameters)
 
-    # execute gradient descent by calling step() of optim
-    optim.step()
+batch_size = 100
+for epoch_cnt in range(100):
 
-    # null your gradients
-    loss.null_gradients()
+    idxs = list(resnet.keys())
+    np.random.shuffle(idxs)
+    for batch_cnt in range(0, len(idxs) // batch_size - 1):
+        batch_indices = idxs[(batch_cnt * batch_size):((batch_cnt + 1) * batch_size)]
+        batch_indices2 = idxs[((batch_cnt + 1) * batch_size):((batch_cnt + 2) * batch_size)]
+        # id1 = np.random.choice(list(resnet.keys()))
+        # print(id1)
+        id1 = batch_indices
+        # while id1 == id2:
+        id2 = batch_indices2
+
+        # print(type(resnet[id1]),type(img_to_caption[id1][0]),type(resnet[id2]))
+        good_image = resnet[id1[0]]
+        bad_image = resnet[id2[0]]
+        text = embed_text.se_text(img_to_caption[id1[0]][0], glove, idfs)
+        for i in id1[1:]:
+            good_image = np.vstack((good_image, resnet[i]))
+            text = np.vstack((text, embed_text.se_text(img_to_caption[i][0], glove, idfs)))
+
+        for i in id2[1:]:
+            bad_image = np.vstack((bad_image, resnet[i]))
+
+            sim_to_good = cos_sim.cos_sim(model(good_image), text)
+            sim_to_bad = cos_sim.cos_sim(model(bad_image), text)
+
+        # compute the loss associated with our predictions(use softmax_cross_entropy)
+        loss = margin_ranking_loss(sim_to_good, sim_to_bad, 1, 0.1)
+        # back-propagate through your computational graph through your loss
+        loss.backward()
+
+        # compute the accuracy between the prediction and the truth
+        acc = accuracy(sim_to_good.data, sim_to_bad.data)
+        # execute gradient descent by calling step() of optim
+        optim.step()
+        # null your gradients
+        loss.null_gradients()
+
+
+    np.save('weight', model.dense1.parameters[0].data)
+    np.save('bias', model.dense1.parameters[1].data)
+
