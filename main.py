0bce818e04a96dfff238fef8f7dfc42362335ca6
Emily Y <emilygbs@yahoo.com>
2019-07-26 10:56:35 -0400

    updated main method with pickle files

--- main.py
+++ main.py
@@ -13,5 +13,12 @@ def main():
     resnet = unpickle.unpickle()
     # make_database.make_database()     # uncomment this only if you want to repickle the files
 
+    with open("idfs1.pkl", mode="rb") as idf:
+        idfs=pickle.load(idf)
+    with open("img_to_caption1.pkl", mode="rb") as cap:
+        img_to_caption=pickle.load(cap)
+    with open("img_to_coco1.pkl", mode="rb") as coco:
+        img_to_coco=pickle.load(coco)
+
 if __name__ == "__main__":
     main()
